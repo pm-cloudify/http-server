be08fb7d6b40aaa6373cdb2100551dfb78f0d76f
Pouya <pouyamohammadyirbu@gmail.com>
2025-03-23 19:47:21 +0330

    fix: postgres compose file and user tables

--- internal/migrations/0001_create_users_table.up.sql
+++ internal/migrations/0001_create_users_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username TEXT NOT NULL,
+    username TEXT UNIQUE NOT NULL,
     email TEXT UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
