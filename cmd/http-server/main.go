24f30f02706a5f0f106b0b3d1ecaec9e185b687c
Pouya <pouyamohammadyirbu@gmail.com>
2025-03-23 10:56:24 +0330

    feat: implementing server configuration

--- cmd/http-server/main.go
+++ cmd/http-server/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/pm-cloudify/http-server/internal/config"
+)
+
+func main() {
+	router := gin.New()
+
+	config.ConfigGinLogger(router)
+	server := config.ConfigGinServer(router)
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	server.ListenAndServe()
+}
---
+++ internal/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ConfigGinServer(router *gin.Engine) *http.Server {
+	return &http.Server{
+		Addr:           ":5000",
+		Handler:        router,
+		ReadTimeout:    5 * time.Second,
+		WriteTimeout:   5 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+}
+
+func ConfigGinLogger(router *gin.Engine) {
+	router.Use(gin.LoggerWithFormatter(func(params gin.LogFormatterParams) string {
+		err_msg := ""
+		if len(params.ErrorMessage) > 0 {
+			err_msg = fmt.Sprintf("| error: %s", params.ErrorMessage)
+		}
+
+		return fmt.Sprintf("%s | %s | %d | %s %s\n",
+			params.ClientIP,
+			params.Method,
+			params.StatusCode,
+			params.Path,
+			err_msg,
+		)
+	}))
+
+	router.Use(gin.Recovery())
+}
